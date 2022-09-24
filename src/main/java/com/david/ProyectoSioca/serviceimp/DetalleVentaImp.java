872d8d017779b595221463fe73e252cd4f4c8d57
DavidValbuena1 <104107409+DavidValbuena1@users.noreply.github.com>
2022-09-24 00:57:12 -0500

    Add files via upload

--- src/main/java/com/david/ProyectoSioca/serviceimp/DetalleVentaImp.java
+++ src/main/java/com/david/ProyectoSioca/serviceimp/DetalleVentaImp.java
@@ -1,19 +1,30 @@
 package com.david.ProyectoSioca.serviceimp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.david.ProyectoSioca.model.DetalleVenta;
+import com.david.ProyectoSioca.model.Producto;
+import com.david.ProyectoSioca.model.Venta;
 import com.david.ProyectoSioca.repository.DetalleVentaRepository;
+import com.david.ProyectoSioca.repository.ProductoRepository;
+import com.david.ProyectoSioca.repository.VentaRepository;
 import com.david.ProyectoSioca.service.DetalleVentaService;
 
 @Component
 public class DetalleVentaImp implements DetalleVentaService {
-	
-	@Autowired(required=true)
+
+	@Autowired(required = true)
 	private DetalleVentaRepository repositorio;
+
+	@Autowired(required = true)
+	private ProductoRepository repositorioProducto;
+
+	@Autowired(required = true)
+	private VentaRepository repositorioVenta;
 
 	@Override
 	public List<DetalleVenta> encontrarTodosLosDetalles() {
@@ -22,6 +33,18 @@ public class DetalleVentaImp implements DetalleVentaService {
 
 	@Override
 	public List<DetalleVenta> insertarDetalles(List<DetalleVenta> lista) {
+		long sumatotal = 0;
+		Producto producto = new Producto();
+		Venta venta = new Venta();
+		for (DetalleVenta dv : lista) {
+			producto = repositorioProducto.findById(dv.getIdproducto().getId_producto());
+			producto.setQuantity(producto.getQuantity() - dv.getCantidad());
+			repositorioProducto.save(producto);
+			sumatotal = sumatotal + dv.getSubtotal();
+			venta = repositorioVenta.findById(dv.getIdventa().getId());
+		}
+		venta.setPrecio(sumatotal);
+		repositorioVenta.save(venta);
 		return repositorio.saveAll(lista);
 	}
 
@@ -32,7 +55,30 @@ public class DetalleVentaImp implements DetalleVentaService {
 
 	@Override
 	public DetalleVenta editarDetalle(DetalleVenta dv) {
-		return repositorio.save(dv);
+		int diferencias=0;
+		long sumatotal = 0;
+		Producto producto = new Producto();
+		Venta venta = new Venta();
+		DetalleVenta dv2 = new DetalleVenta();
+		producto = repositorioProducto.findById(dv.getIdproducto().getId_producto());
+		if(dv2.getCantidad()>dv.getCantidad()) {
+			diferencias=dv2.getCantidad()-dv.getCantidad();
+			producto.setQuantity(producto.getQuantity() + diferencias);
+		}else if(dv2.getCantidad()<dv.getCantidad()) {
+			diferencias=dv.getCantidad()-dv2.getCantidad();
+			producto.setQuantity(producto.getQuantity() - diferencias);
+		}	
+		dv2 = repositorio.save(dv);
+		List<DetalleVenta> lista = new ArrayList<>();
+		lista = repositorio.findAll();
+		for (DetalleVenta dv3 : lista) {
+			sumatotal = sumatotal + dv3.getSubtotal();
+		}
+		venta = repositorioVenta.findById(dv.getIdventa().getId());
+		venta.setPrecio(sumatotal);
+		repositorioProducto.save(producto);
+		repositorioVenta.save(venta);
+		return dv2;
 	}
 
 	@Override
