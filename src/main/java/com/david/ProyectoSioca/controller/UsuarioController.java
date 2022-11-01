09295ed7a5696e0ef6c52cb83e556a136e8c5150
DavidValbuena1 <104107409+DavidValbuena1@users.noreply.github.com>
2022-10-31 22:00:42 -0500

    Add files via upload

--- src/main/java/com/david/ProyectoSioca/controller/UsuarioController.java
+++ src/main/java/com/david/ProyectoSioca/controller/UsuarioController.java
@@ -24,90 +24,91 @@ import com.david.ProyectoSioca.model.Usuario;
 import com.david.ProyectoSioca.service.RecuperarContraseñaService;
 import com.david.ProyectoSioca.service.UsuarioService;
 
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping({"/Sioca/Usuario"})
+@RequestMapping({ "/Sioca/Usuario" })
 public class UsuarioController {
-	
-	@Autowired(required=true)
-	private UsuarioService service;
-	
-	@Autowired(required=true)
-	private RecuperarContraseñaService RecuperarService;
-	
-	@GetMapping(path= {"/listar"})
-	public List<Usuario> listar(){
-		return service.encontrarTodosLosUsuarios();
-	}
-	
-	@GetMapping(path= {"/listarId/{id}"})
-	public Usuario encontrarUsuario(@PathVariable("id") int id) {
-		System.out.println(service.encontrarUsuarioPorId(id));
-		return service.encontrarUsuarioPorId(id);
-	}
-	
-	@PostMapping(path= {"/agregar"})
-	public Usuario insertarUsuario(@RequestBody Usuario u) {
-		return service.insertarUsuario(u);
-	}
-	
-	@PutMapping(path= {"/editar/{id}"})
-	public Usuario editarUsuario(@RequestBody Usuario u, @PathVariable("id") int id) {
-		u.setId_usuario(id);
-		return service.editarUsuario(u);
-	}
-	
-	@DeleteMapping(path= {"/eliminar/{id}"})
-	public Usuario eliminarUsuario(@PathVariable("id") int id) {
-		return service.borrarUsuarioPorId(id);
-	}
-	
-	@PostMapping(path= {"/verificar"})
-	public Usuario verificar(@RequestBody Usuario u) {
-		return service.verificar(u.getCorreo(), u.getContrasena());
-	}
-	
-	
-	@PostMapping(path={"/recuperar"})
-	public ResponseEntity<Usuario> recuperar(@RequestBody Usuario u) {
-		String[] letters = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-		String[] numbers = {"0","1","2","3","4","5","6","7","8","9"};
-		String color = "";
-		for (int i = 0; i < 3; i++ ) {
-			color += letters[(int) Math.round(Math.random() * 25)];
-			color += numbers[(int) Math.round(Math.random() * 9)];
-		}
-		u=service.recuperar(u.getCorreo(),color);
-		if(u!=null) {
-			RecuperarContraseña r = new RecuperarContraseña();
-			r.setUsuario(u);
-			r.setCodigorecuperacion(color);
-			r.setEstado((byte) 1);
-			RecuperarService.insertar(r);
-			return new ResponseEntity<Usuario>(u,null,HttpStatus.CREATED);
-		}
-		return new ResponseEntity<Usuario>(u,null,HttpStatus.NOT_FOUND);
-	}
 
-	@PostMapping(path= {"/validarCodigo"})
-	public Usuario validarCodigo (@RequestBody RecuperarContraseña r) throws ParseException {
-		Usuario u = new Usuario();
-		RecuperarContraseña recuperarContraseña = new RecuperarContraseña();
-		recuperarContraseña = RecuperarService.validar(r.getCodigorecuperacion(), r.getUsuario().getCorreo());
-		if(recuperarContraseña!=null) {
-			u = service.encontrarUsuarioPorId(recuperarContraseña.getUsuario().getId_usuario());
-			r.setEstado((byte) 0);
-			DateFormat hoy = new SimpleDateFormat("yyyy/MM/dd HH-mm-ss");   
-			String fecha = hoy.format(new Date()).toString();
-			Date date = hoy.parse(fecha);
-			System.out.println("hola");
-			System.out.println(date);
-			r.setHoradecreacion(date);
-			RecuperarService.insertar(recuperarContraseña);
-		}else {
-			u=null;
-		}
-		return u;
-	}
-	
+    @Autowired(required = true)
+    private UsuarioService service;
+
+    @Autowired(required = true)
+    private RecuperarContraseñaService RecuperarService;
+
+    @GetMapping(path = { "/listar" })
+    public List<Usuario> listar() {
+        return service.encontrarTodosLosUsuarios();
+    }
+
+    @GetMapping(path = { "/listarId/{id}" })
+    public Usuario encontrarUsuario(@PathVariable("id") int id) {
+        System.out.println(service.encontrarUsuarioPorId(id));
+        return service.encontrarUsuarioPorId(id);
+    }
+
+    @PostMapping(path = { "/agregar" })
+    public Usuario insertarUsuario(@RequestBody Usuario u) {
+        return service.insertarUsuario(u);
+    }
+
+    @PutMapping(path = { "/editar/{id}" })
+    public Usuario editarUsuario(@RequestBody Usuario u, @PathVariable("id") int id) {
+        u.setId_usuario(id);
+        return service.editarUsuario(u);
+    }
+
+    @DeleteMapping(path = { "/eliminar/{id}" })
+    public Usuario eliminarUsuario(@PathVariable("id") int id) {
+        return service.borrarUsuarioPorId(id);
+    }
+
+    @PostMapping(path = { "/verificar" })
+    public Usuario verificar(@RequestBody Usuario u) {
+        return service.verificar(u.getCorreo(), u.getContrasena());
+    }
+
+    @PostMapping(path = { "/recuperar" })
+    public ResponseEntity<Usuario> recuperar(@RequestBody Usuario u) throws ParseException {
+        String[] letters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+                "S", "T", "U", "V", "W", "X", "Y", "Z" };
+        String[] numbers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        String color = "";
+        for (int i = 0; i < 3; i++) {
+            color += letters[(int) Math.round(Math.random() * 25)];
+            color += numbers[(int) Math.round(Math.random() * 9)];
+        }
+        u = service.recuperar(u.getCorreo(), color);
+        if (u != null) {
+            RecuperarContraseña r = new RecuperarContraseña();
+            r.setUsuario(u);
+            r.setCodigorecuperacion(color);
+            r.setEstado((byte) 1);
+            DateFormat hoy = new SimpleDateFormat("yyyy/MM/dd HH-mm-ss");   
+            String fecha = hoy.format(new Date()).toString();
+            Date date;
+            date = hoy.parse(fecha);
+            System.out.println("hola");
+            System.out.println(date);
+            r.setHoradecreacion(date);
+            RecuperarService.insertar(r);
+            return new ResponseEntity<Usuario>(u, null, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<Usuario>(u, null, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(path = { "/validarCodigo" })
+    public Usuario validarCodigo(@RequestBody RecuperarContraseña r) {
+        Usuario u = new Usuario();
+        RecuperarContraseña recuperarContraseña = new RecuperarContraseña();
+        recuperarContraseña = RecuperarService.validar(r.getCodigorecuperacion(), r.getUsuario().getCorreo());
+        if (recuperarContraseña != null) {
+            u = service.encontrarUsuarioPorId(recuperarContraseña.getUsuario().getId_usuario());
+            recuperarContraseña.setEstado((byte) 0);
+            RecuperarService.insertar(recuperarContraseña);
+        } else {
+            u = null;
+        }
+        return u;
+    }
+
 }
